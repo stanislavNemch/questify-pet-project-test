fca18c19dd02313689dbc307ad6fda3aebc8f241
Stanislav Nemchenko <stas.buka@gmail.com>
2025-09-24 00:54:09 +0300

    refactor: Update QuestGroup component to use new icon library and adjust CSS styles for improved layout

--- src/components/QuestGroup/QuestGroup.tsx
+++ src/components/QuestGroup/QuestGroup.tsx
@@ -1,35 +1,35 @@
 import React from "react";
 import type { ReactNode } from "react";
 import css from "./QuestGroup.module.css";
-import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+import { TiArrowSortedUp, TiArrowSortedDown } from "react-icons/ti";
 
 // Описываем типы для наших пропсов
 interface QuestGroupProps {
-  title: string;
-  isOpen: boolean;
-  onToggle: () => void;
-  children: ReactNode; // children может быть любым валидным React-элементом
+    title: string;
+    isOpen: boolean;
+    onToggle: () => void;
+    children: ReactNode; // children может быть любым валидным React-элементом
 }
 
 const QuestGroup: React.FC<QuestGroupProps> = ({
-  title,
-  isOpen,
-  onToggle,
-  children,
+    title,
+    isOpen,
+    onToggle,
+    children,
 }) => {
-  return (
-    <div className={css.questGroup}>
-      <div className={css.groupHeader} onClick={onToggle}>
-        <h2 className={css.groupTitle}>{title}</h2>
-        <button className={css.toggleButton}>
-          {isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
-        </button>
-        <div className={css.dividerLine}></div>
-      </div>
-      {/* Показываем дочерние элементы только если секция открыта */}
-      {isOpen && <div className={css.cardGrid}>{children}</div>}
-    </div>
-  );
+    return (
+        <div className={css.questGroup}>
+            <div className={css.groupHeader} onClick={onToggle}>
+                <h2 className={css.groupTitle}>{title}</h2>
+                <button className={css.toggleButton}>
+                    {isOpen ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
+                </button>
+                <div className={css.dividerLine}></div>
+            </div>
+            {/* Показываем дочерние элементы только если секция открыта */}
+            {isOpen && <div className={css.cardGrid}>{children}</div>}
+        </div>
+    );
 };
 
 export default QuestGroup;
